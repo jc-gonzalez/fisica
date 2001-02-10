ca9efd9958d5a2baab6994039f1639e9dee582ea
gonzalez <>
2001-02-10 13:02:55 +0000

    new project started

---
+++ CVSROOT/qmagicdisplay/qmagicdisplay/qmagicdisplayview.cpp
@@ -0,0 +1,47 @@
+/*************************************************************************
+ *                                                                          
+ * qmagicdisplayview.cpp  -  description
+ *
+ * Copyright (C) 2001  J C Gonzalez
+ * gonzalez@gae.ucm.es
+ *
+ *------------------------------------------------------------------------
+ *
+ * Copyright (C) 2001 J C Gonzalez
+ *  
+ * This program is free software;  you can redistribute it and/or  modify
+ * it under the terms  of the GNU General  Public License as published by
+ * the Free Software Foundation; either version  2 of the License, or (at
+ * your option) any later version.
+ * 
+ * This piece of code is distributed in the hope  that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * 
+ * In no  event shall his author  be liable for  any special, incidental,
+ * indirect  or  consequential  damages  of any  kind,   or  any  damages
+ * whatsoever resulting from loss of use, data or profits, whether or not
+ * advised of the possibility of damage, and on  any theory of liability,
+ * arising out  of or in connection  with the use  or performance of this
+ * software. You've been warned.
+ * 
+ ************************************************************************/
+
+
+#include "qmagicdisplayview.h"
+
+QMAGICDisplayView::QMAGICDisplayView(QWidget *parent, QMAGICDisplayDoc *doc) : QWidget(parent)
+{
+  /** connect doc with the view*/
+  connect(doc, SIGNAL(documentChanged()), this, SLOT(slotDocumentChanged()));
+}
+
+QMAGICDisplayView::~QMAGICDisplayView()
+{
+}
+
+void QMAGICDisplayView::slotDocumentChanged()
+{
+  //TODO update the view
+
+}
---
+++ CVSROOT/qmagicdisplay/qmagicdisplay/resource.h
@@ -0,0 +1,85 @@
+/*************************************************************************
+ *                                                                          
+ * resource.h  -  description
+ *
+ * Copyright (C) 2001  J C Gonzalez
+ * gonzalez@gae.ucm.es
+ *
+ *------------------------------------------------------------------------
+ *
+ * Copyright (C) 2001 J C Gonzalez
+ *  
+ * This program is free software;  you can redistribute it and/or  modify
+ * it under the terms  of the GNU General  Public License as published by
+ * the Free Software Foundation; either version  2 of the License, or (at
+ * your option) any later version.
+ * 
+ * This piece of code is distributed in the hope  that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * 
+ * In no  event shall his author  be liable for  any special, incidental,
+ * indirect  or  consequential  damages  of any  kind,   or  any  damages
+ * whatsoever resulting from loss of use, data or profits, whether or not
+ * advised of the possibility of damage, and on  any theory of liability,
+ * arising out  of or in connection  with the use  or performance of this
+ * software. You've been warned.
+ * 
+ ************************************************************************/
+
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+
+///////////////////////////////////////////////////////////////////
+// resource.h  -- contains macros used for commands
+
+
+///////////////////////////////////////////////////////////////////
+// COMMAND VALUES FOR MENUBAR AND TOOLBAR ENTRIES
+
+
+///////////////////////////////////////////////////////////////////
+// File-menu entries
+#define ID_FILE_NEW                 10020
+#define ID_FILE_OPEN                10030
+
+#define ID_FILE_SAVE                10050
+#define ID_FILE_SAVE_AS             10060
+#define ID_FILE_CLOSE               10070
+
+#define ID_FILE_PRINT               10080
+
+#define ID_FILE_QUIT                10100
+
+
+///////////////////////////////////////////////////////////////////
+// Edit-menu entries
+#define ID_EDIT_UNDO                11010
+#define ID_EDIT_REDO                11020
+#define ID_EDIT_COPY                11030
+#define ID_EDIT_CUT                 11040
+#define ID_EDIT_PASTE               11050
+#define ID_EDIT_SELECT_ALL          11060
+
+
+///////////////////////////////////////////////////////////////////
+// View-menu entries                    
+#define ID_VIEW_TOOLBAR             12010
+#define ID_VIEW_STATUSBAR           12020
+
+///////////////////////////////////////////////////////////////////
+// Help-menu entries
+#define ID_HELP_ABOUT               1002
+
+///////////////////////////////////////////////////////////////////
+// General application values
+#define IDS_APP_ABOUT               "QMAGICDisplay\nVersion " VERSION \
+                                    "\n(w) 2001 by J C Gonzalez"
+#define IDS_STATUS_DEFAULT          "Ready."
+
+#endif // RESOURCE_H
