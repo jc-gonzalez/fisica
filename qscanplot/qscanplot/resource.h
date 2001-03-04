a62cfdfe54a18933def9e92c721c8ecdd8b0bdc2
gonzalez <>
2001-03-04 21:16:58 +0000

    new project started

--- qscanplot/qscanplot/resource.h
+++ qscanplot/qscanplot/resource.h
@@ -1,19 +1,32 @@
-/***************************************************************************
-                          resource.h  -  description
-                             -------------------
-    begin                : Fri Oct 13 19:38:27 CEST 2000
-    copyright            : (C) 2000 by J C Gonzalez
-    email                : gonzalez@gae.ucm.es
- ***************************************************************************/
+/*************************************************************************
+ *                                                                          
+ * resource.h  -  description
+ *
+ * Copyright (C) 2001  J C Gonzalez
+ * gonzalez@gae.ucm.es
+ *
+ *------------------------------------------------------------------------
+ *
+ * Copyright (C) 2001 J C Gonzalez
+ *  
+ * This program is free software;  you can redistribute it and/or  modify
+ * it under the terms  of the GNU General  Public License as published by
+ * the Free Software Foundation; either version  2 of the License, or (at
+ * your option) any later version.
+ * 
+ * This piece of code is distributed in the hope  that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * 
+ * In no  event shall his author  be liable for  any special, incidental,
+ * indirect  or  consequential  damages  of any  kind,   or  any  damages
+ * whatsoever resulting from loss of use, data or profits, whether or not
+ * advised of the possibility of damage, and on  any theory of liability,
+ * arising out  of or in connection  with the use  or performance of this
+ * software. You've been warned.
+ * 
+ ************************************************************************/
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
@@ -66,7 +79,7 @@
 ///////////////////////////////////////////////////////////////////
 // General application values
 #define IDS_APP_ABOUT               "QScanPlot\nVersion " VERSION \
-                                    "\n(w) 2000 by J C Gonzalez"
+                                    "\n(w) 2001 by J C Gonzalez"
 #define IDS_STATUS_DEFAULT          "Ready."
 
 #endif // RESOURCE_H
