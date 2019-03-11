2d1fbf982e9d91232ea70e0255ef175a89b023db
J C Gonzalez <JCGonzalez@sciops.esa.int>
2019-03-11 17:25:11 +0100

    First test on linking with libraries installed

--- CDetSim/src/main.cpp
+++ CDetSim/src/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <cmath>
+
+#include "mathtools.h"
+
+using namespace MathTools;
 
 int main(int argc, char * argv[]) 
 {
+    double m = M_PI;
+    double m2 = sqr<double>(m);
 
-  return 0;
+    std::cout << m << " squared is " << m2 << '\n';
+    return 0;
 }
 
 
